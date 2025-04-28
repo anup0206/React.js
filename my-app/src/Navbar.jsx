24a57459febdaab7de1ded3ef49ba52ea044ea5a
anup0206 <alinedtwins@gmail.com>
2025-04-28 09:07:27 +0545

    minor changes

--- my-app/src/Navbar.jsx
+++ my-app/src/Navbar.jsx
@@ -3,15 +3,16 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-    <h1 className="text-3xl font-bold text-left py-4 bg-gray-800 text-white">
-      Navbar
-    </h1>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Dropdown</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Contact</a></li>
+    
+    <ul >
+        <h1 className="text-3xl font-bold text-left py-4 bg-gray-800 text-white">
+      Flowbite
+        </h1>
+        <li className="text-3xl font-bold text-right py-2bg-gray-800 text-white"><a href="#">Home</a></li>
+        <li className="text-3xl font-bold text-right py-2bg-gray-800 text-white"><a href="#">Dropdown</a></li>
+        <li className="text-3xl font-bold text-right py-2bg-gray-800 text-white"><a href="#">Services</a></li>
+        <li className="text-3xl font-bold text-right py-2bg-gray-800 text-white"><a href="#">Pricing</a></li>
+        <li className="text-3xl font-bold text-right py-2bg-gray-800 text-white"><a href="#">Contact</a></li>
     </ul>
     </div>
   );
