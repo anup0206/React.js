94a3f01e39a410241b55152ed03e9f6025b73a7d
anup0206 <alinedtwins@gmail.com>
2025-04-28 08:59:27 +0545

    minor changes

--- my-app/src/Navbar.jsx
+++ my-app/src/Navbar.jsx
@@ -1,10 +1,20 @@
-
 import React from "react";
 
-const Navbar =()=>{
-    return(
-        <h1 className="">Navbar </h1>
-    );
-}
+const Navbar = () => {
+  return (
+    <div>
+    <h1 className="text-3xl font-bold text-left py-4 bg-gray-800 text-white">
+      Navbar
+    </h1>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Dropdown</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    </div>
+  );
+};
 
 export default Navbar;
