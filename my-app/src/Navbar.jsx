3482649be70e9fc833cc8a900d8f9fbf5a3f5efe
anup0206 <alinedtwins@gmail.com>
2025-04-28 09:31:31 +0545

    code in minimum no. of line

--- my-app/src/Navbar.jsx
+++ my-app/src/Navbar.jsx
@@ -7,22 +7,15 @@ const Navbar = () => {
         <h1 className="text-3xl font-bold text-left py-4 text-white w-full">
           Flowbite
         </h1>
-        <li className="text-3xl font-bold text-right py-2 text-white w-full">
-          <a href="#">Home</a>
-        </li>
-        
-        <li className="text-3xl font-bold text-right py-2 text-white w-full">
-          <a href="#">Dropdown</a>
-        </li>
-        <li className="text-3xl font-bold text-right py-2 text-white w-full">
-          <a href="#">Services</a>
-        </li>
-        <li className="text-3xl font-bold text-right py-2 text-white w-full">
-          <a href="#">Pricing</a>
-        </li>
-        <li className="text-3xl font-bold text-right py-2 text-white w-full">
-          <a href="#">Contact</a>
-        </li>
+        {["Home", "Dropdown", "Services", "Pricing", "Contact"].map((item, index) => (
+          <li
+            key={index}
+            className="text-3xl font-bold text-right py-2 text-white w-full 
+              hover:text-yellow-400 transition duration-300"
+          >
+            <a href="#">{item}</a>
+          </li>
+        ))}
       </ul>
     </div>
   );
