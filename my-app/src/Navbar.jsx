f766fbae2bf128750b302b8a381ab692af60fd9b
anup0206 <alinedtwins@gmail.com>
2025-04-28 09:12:43 +0545

    minor changes

--- my-app/src/Navbar.jsx
+++ my-app/src/Navbar.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="bg-gray-800">
       <ul className="flex flex-col md:flex-row md:space-x-4 items-start md:items-center">
-        <h1 className="text-3xl font-bold text-left py-4 bg-gray-800 text-white w-full">
+        <h1 className="text-3xl font-bold text-left py-4 text-white w-full">
           Flowbite
         </h1>
-        <li className="text-3xl font-bold text-right py-2 bg-gray-800 text-white w-full">
+        <li className="text-3xl font-bold text-right py-2 text-white w-full">
           <a href="#">Home</a>
         </li>
-        <li className="text-3xl font-bold text-right py-2 bg-gray-800 text-white w-full">
+        <li className="text-3xl font-bold text-right py-2 text-white w-full">
           <a href="#">Dropdown</a>
         </li>
-        <li className="text-3xl font-bold text-right py-2 bg-gray-800 text-white w-full">
+        <li className="text-3xl font-bold text-right py-2 text-white w-full">
           <a href="#">Services</a>
         </li>
-        <li className="text-3xl font-bold text-right py-2 bg-gray-800 text-white w-full">
+        <li className="text-3xl font-bold text-right py-2 text-white w-full">
           <a href="#">Pricing</a>
         </li>
-        <li className="text-3xl font-bold text-right py-2 bg-gray-800 text-white w-full">
+        <li className="text-3xl font-bold text-right py-2 text-white w-full">
           <a href="#">Contact</a>
         </li>
       </ul>
